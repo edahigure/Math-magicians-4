0099da8d8216ef36be3406f80ca43dd543e5ffcc
Edahí Antonio Gutiérrez Reyes <edahigure@gmail.com>
2023-02-24 10:46:33 -0700

    Fix Routs

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,12 @@ import Quote from './pages/Quote';
 function App() {
   return (
     <>
+    <Layout />
       <Routes>
-        <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="Home" element={<Home />} />
           <Route path="Calculator" element={<Calculator />} />
           <Route path="Quote" element={<Quote />} />
-        </Route>
-
       </Routes>
     </>
 
