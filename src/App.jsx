e0de26826caa54b223a69bdade8915fe138bac08
Edahí Antonio Gutiérrez Reyes <edahigure@gmail.com>
2023-02-23 22:22:59 -0700

    Fix linters

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,19 @@ import './App.css';
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Calculator from './components/Calculator';
-import Layout from './pages/Layout'
-import Home from './pages/Home'
-import Quote from './pages/Quote'
+import Layout from './pages/Layout';
+import Home from './pages/Home';
+import Quote from './pages/Quote';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<Layout />} >
-          <Route path='/Home' element={<Home />} />
-          <Route path='/Calculator' element={<Calculator />} />
-          <Route path='/Quote' element={<Quote />} />
+        <Route path="/" element={<Layout />}>
+          <Route path="/Home" element={<Home />} />
+          <Route path="/Calculator" element={<Calculator />} />
+          <Route path="/Quote" element={<Quote />} />
         </Route>
-
 
       </Routes>
     </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,38 @@
 import React from 'react';
 
-const Home = () => {
-  return (
-    <>
-      <div className="wrapper-3">
-        <h1> Welcome to our page!</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dignissim neque ac dolor tempus sodales. Quisque facilisis tellus eu neque varius, imperdiet aliquam ante hendrerit. Sed sed risus facilisis, fringilla ligula vitae, elementum metus. Nunc bibendum viverra interdum. Vestibulum condimentum luctus massa vel iaculis. Aliquam maximus eros odio, tristique tincidunt leo volutpat sit amet. Nunc scelerisque, justo et fermentum consequat, sem neque consectetur orci, at gravida leo tellus id turpis. Integer ante orci, molestie a malesuada in, ornare vel justo. In vitae risus cursus, feugiat nulla id, faucibus augue. Sed nunc risus, faucibus vitae dictum id, pharetra eget est. Vivamus sed ipsum non nisl porttitor euismod et non quam. Donec consectetur, velit et rhoncus commodo, tortor tortor imperdiet lectus, quis tincidunt nulla libero vitae lacus. Suspendisse eget tortor nibh. Nulla dapibus, nisi et accumsan faucibus, lorem justo dapibus erat, at luctus libero ipsum vitae quam. Integer vel ligula scelerisque, feugiat dolor euismod, fringilla magna.</p>
-        <p>Sed viverra condimentum sem sed rutrum. Nunc mollis consequat lectus venenatis feugiat. Ut molestie ante quis sapien tempor lacinia. Ut quis elit sed ex porta eleifend. Duis maximus ullamcorper imperdiet. Quisque vitae lobortis mauris, eget pretium ipsum. Aenean a euismod orci. </p>
-      </div>
+const Home = () => (
+  <>
+    <div className="wrapper-3">
+      <h1> Welcome to our page!</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dignissim neque ac dolor
+        tempus sodales. Quisque facilisis tellus eu neque varius, imperdiet aliquam ante
+        hendrerit. Sed sed risus facilisis, fringilla ligula vitae, elementum metus. Nunc bibendum
+        viverra interdum. Vestibulum condimentum luctus massa vel iaculis. Aliquam maximus eros
+        odio, tristique tincidunt leo volutpat sit amet. Nunc scelerisque, justo et fermentum
+        consequat, sem neque consectetur orci, at gravida leo tellus id turpis.
+        Integer ante orci, molestie a malesuada in, ornare vel justo. In vitae risus cursus,
+        feugiat nulla id,
+        faucibus augue. Sed nunc risus, faucibus vitae dictum id, pharetra eget est. Vivamus sed
+        ipsum non
+        nisl porttitor euismod et non quam. Donec consectetur, velit et rhoncus commodo, tortor
+        tortor
+        imperdiet lectus, quis tincidunt nulla libero vitae lacus. Suspendisse eget tortor nibh.
+        Nulla
+        dapibus, nisi et accumsan faucibus, lorem justo dapibus erat, at luctus libero ipsum
+        vitae quam.
+        Integer vel ligula scelerisque, feugiat dolor euismod, fringilla magna.
+      </p>
+      <p>
+        Sed viverra condimentum sem sed rutrum. Nunc mollis consequat lectus venenatis feugiat.
+        Ut molestie ante quis sapien tempor lacinia. Ut quis elit sed ex porta eleifend. Duis
+        maximus
+        ullamcorper imperdiet. Quisque vitae lobortis mauris, eget pretium ipsum. Aenean a
+        euismod orci.
+        {' '}
+      </p>
+    </div>
 
-    </>
-  );
-}
+  </>
+);
 export default Home;
